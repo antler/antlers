3af82c0067aaf71cd623cb7fbd8593b4dc704936
ryan spangler <ryan.spangler@gmail.com>
2012-08-02 14:37:17 -0700

    added test for blocks

--- test/stencil/test/core.clj
+++ test/stencil/test/core.clj
@@ -1,6 +1,7 @@
 (ns stencil.test.core
   (:use clojure.test
-        stencil.core))
+        stencil.core
+        stencil.parser))
 
 ;; Test case to make sure we don't get a regression on inverted sections with
 ;; list values for a name.
@@ -10,3 +11,11 @@
          (render-string "{{^a}}a{{b}}a{{/a}}" {:a [:b "11"]})))
   (is (= ""
          (render-string "{{^a}}a{{b}}a{{/a}}" {"a" ["b" "11"]}))))
+
+
+(def base "x{{%yellow}}{{/yellow}}z")
+
+(deftest block-test
+  (register-template "base" base)
+  (is (= "xyz"
+         (render-string "{{< base}}{{%yellow}}{{yyy}}{{/yellow}}" {:yyy "y"}))))
